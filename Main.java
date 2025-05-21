ea3d24a5b46618fedb05e72fa9c3c961cec8c518
Mats-Luca Dagott <mats.luca.dagott@gmail.com>
2025-05-21 12:35:32 +0200

    Multiplikation umgesetzt

--- Main.java
+++ Main.java
@@ -7,5 +7,15 @@
  */
 public class Main
 {
-    
+    public static void main(String args[]){
+        int obereGrenze = 100;
+        int mult = 1;
+        
+        for(int i = 1; 1 <= obereGrenze; ++i){
+            mult *= i;
+        }
+        
+        System.out.println("Mult = " + mult);
+        
+    }
 }
